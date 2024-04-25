fa2362fbe4119944eee95a5ea2cb81ccfec63b75
yalla12@naver.com <yalla12@naver.com>
2024-04-25 16:24:40 +0900

    java 1.8 사용을 위한 spring boot 버전 2.x 버전으로 다운 그레이드

--- src/main/java/thecommerceproject/domain/Member.java
+++ src/main/java/thecommerceproject/domain/Member.java
@@ -1,9 +1,11 @@
 package thecommerceproject.domain;
 
-import jakarta.persistence.*;
+
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import javax.persistence.*;
 
 @Getter
 @Setter
