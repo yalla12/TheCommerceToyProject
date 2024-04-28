c77529f0600f3fd7d5711984be4a7db75a50c935
yalla12@naver.com <yalla12@naver.com>
2024-04-28 17:45:21 +0900

    swagger 연동을 통한 api문서 기능 개발

--- src/main/java/thecommerceproject/controller/MemberController.java
+++ src/main/java/thecommerceproject/controller/MemberController.java
@@ -1,5 +1,7 @@
 package thecommerceproject.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +14,7 @@ import thecommerceproject.service.MemberService;
 import javax.validation.Valid;
 import java.net.URI;
 
+@Api(tags = "member API")
 @RestController
 @RequiredArgsConstructor
 public class MemberController {
@@ -23,6 +26,7 @@ public class MemberController {
      * @param memberRequestDto 입력한 회원 정보
      * @return ResponseEntity
      */
+    @ApiOperation(value = "회원 가입")
     @PostMapping("/member/create")
     public ResponseEntity createMember(@Valid @RequestBody MemberRequestDto memberRequestDto) throws Exception {
         memberService.createMember(memberRequestDto);
@@ -36,6 +40,7 @@ public class MemberController {
      * @param sort 정렬 기준 0일때 가입일 순, 1일때 이름순
      * @return Page<Member> 페이지 설정에 해당 되는 회원 리스트
      */
+    @ApiOperation(value = "회원 목록 조회")
     @GetMapping("/member/search")
     public Page<Member> searchMember(@RequestParam("page") int page,@RequestParam("pageSize") int pageSize,@RequestParam("sort") int sort) {
         return memberService.searchMember(page,pageSize, sort);
@@ -47,6 +52,7 @@ public class MemberController {
      * @param updateMemberDto 수정 정보
      * @return Member 수정된 회원 정보
      */
+    @ApiOperation(value = "회원 정보 수정")
     @PutMapping("/member/update/{memberId}")
     public Member updateMember(@PathVariable String memberId, @Valid @RequestBody UpdateMemberDto updateMemberDto) {
         return memberService.updateMember(memberId, updateMemberDto);
